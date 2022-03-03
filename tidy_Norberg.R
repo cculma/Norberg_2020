a713b92efe88a64764dadb60b701f99616e9a984
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-03-03 12:43:25 -0800

    files mr bean 1

--- tidy_Norberg.R
+++ tidy_Norberg.R
@@ -15,3 +15,19 @@ a3[,lev1] <- lapply(a3[,lev1], factor)
 summary(a3)
 
 write.csv(a3, "Y_PH_FD1.csv", row.names = F, quote = F)
+
+
+str(a3)
+N_list <- as.factor(c(a3$Location, a3$Year, a3$Cut))
+length(N_list)
+a4 <- a3 %>% unite("merged", c(Location, Year, Cut), sep = "_", remove = F)
+a4$merged <- as.factor(a4$merged)
+str(a4)
+a5 <- split(a4, a4$merged)
+names(a5)
+
+setwd("~/Documents/Cesar/git/Norberg_2020/mr_blue1/")
+for (i in names(a5)) {
+  write.csv(a5[[i]],  paste0(i, '.csv'), row.names = F, quote = F)
+}
+
