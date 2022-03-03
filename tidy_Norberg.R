31ae2476921cd05e3394e76061fe0190b26a094a
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-03-03 11:29:32 -0800

    read files

--- tidy_Norberg.R
+++ tidy_Norberg.R
@@ -1,3 +1,9 @@
-# tidy data
+# tidy data to add rows and cols in yield (Y), plant height (PH) and fall dormancy (FD)
 rm(list = ls())
 library(tidyverse)
+
+a1 <- read.csv("cols_rows1.csv")
+a2 <- read.csv("Y_PH_FD.csv")
+
+colnames(a1)
+colnames(a2)
