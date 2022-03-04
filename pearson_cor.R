54d0f7c7008bc8715e5049d9ea6bb112e31be4a8
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-03-04 09:41:30 -0800

    correlation

--- pearson_cor.R
+++ pearson_cor.R
@@ -5,21 +5,24 @@ library(tidyverse)
 library(data.table)
 
 #################
-
 # 1 stage results
 
 a1 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA/4_Yield/1_stage/predictions_2stage_ASReml_mrbean.csv")
 a2 <- a1 %>% dplyr::select(1:3) %>% spread(trial, predicted.value) %>% remove_rownames() %>% column_to_rownames(var = "gen")
 list_5 <- gsub("BLUE_", "ST1_", colnames(a2))
 colnames(a2) <- list_5
+a2 <- a2 %>% rownames_to_column(var = "gen")
 
+#################
 # Augmented results
 
 b1 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE2/4_Yield_1stage.csv")
 b2 <- b1 %>% dplyr::select(1:3) %>% spread(merged, predicted.value) %>% remove_rownames() %>% column_to_rownames(var = "gen")
 list_6 <- gsub("BLUE_", "AUG_", colnames(b2))
 colnames(b2) <- list_6
+b2 <- b2 %>% rownames_to_column(var = "gen")
 
+################
 # 2 stage results (six measures)
 
 setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA/4_Yield/2_stage/")
@@ -37,8 +40,9 @@ c1 <-rbindlist(c1, use.names=TRUE, fill=TRUE, idcol="merged")
 c2 <- c1 %>% dplyr::select(2:4) %>% spread(trial, predicted.value) %>% remove_rownames() %>% column_to_rownames(var = "gen")
 list_7 <- gsub("BLUE_", "ST2_", colnames(c2))
 colnames(c2) <- list_7
+c2 <- c2 %>% rownames_to_column(var = "gen")
 
-########
+################
 # 3 stage results (three locations)
 
 setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA/4_Yield/3_stage/")
@@ -54,5 +58,18 @@ for (i in 1:length(data_3st)) {
 names(d1) <- list_4
 d1 <-rbindlist(d1, use.names=TRUE, fill=TRUE, idcol="merged")
 d2 <- d1 %>% dplyr::select(2:4) %>% spread(trial, predicted.value) %>% remove_rownames() %>% column_to_rownames(var = "gen")
-list_7 <- gsub("BLUE_", "ST2_", colnames(c2))
-colnames(c2) <- list_7
+list_8 <- gsub("BLUE_", "ST3_", colnames(d2))
+colnames(d2) <- list_8
+d2 <- d2 %>% rownames_to_column(var = "gen")
+
+################
+
+e1 <- inner_join(b2, a2, by = "gen") %>% inner_join(., c2, by = "gen") %>% inner_join(., d2, by = "gen") %>% remove_rownames() %>% column_to_rownames(var = "gen")
+colnames(e1)
+e2 <- e1[,c(22,44,66,88)]
+e2 <- e1[,c(1,23,45,67)]
+
+library(corrplot)
+e2 <- cor(e2, use = "complete.obs")
+corrplot(e2, type="upper", method = 'number')
+
