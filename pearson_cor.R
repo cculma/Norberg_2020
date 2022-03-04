3761cb25f6c55288f839143af295f6b44221ab59
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-03-04 08:24:21 -0800

    pearson 1

--- pearson_cor.R
+++ pearson_cor.R
@@ -1,2 +1,27 @@
 # import multi-stage analysis and correlate data
 
+rm(list = ls())
+library(tidyverse)
+library(data.table)
+
+#################
+# 1 stage
+
+setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA/4_Yield/1_stage/")
+data_Yield <- list.files(pattern = ".csv", full.names = T)
+list_4 <- gsub(".csv", "", gsub("./", "", data_Yield))
+
+Yield_1 <- list()
+for (i in 1:length(data_Yield)) {
+  data <- read.csv(data_Yield[i])
+  data <- data[,c(1:3)]
+  Yield_1[[length(Yield_1)+1]] = data
+}
+
+a1 <- read.csv("predictions_2stage_ASReml_mrbean.csv")
+a1 <- a1[,c(1:3)]
+spread()
+
+names(Yield_1) <- list_4
+Yield_2 <-rbindlist(Yield_1, use.names=TRUE, fill=TRUE, idcol="merged")
+write.csv(Yield_2, "~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE2/Yield_1stage.csv", row.names = F, quote = F)
--- tidy_MET.R
+++ tidy_MET.R
@@ -9,7 +9,7 @@ library(data.table)
 #################
 # MSC
 
-setwd("~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE1/1_MSC/")
+setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE1/1_MSC/")
 data_MSC <- list.files(pattern = ".csv", full.names = T)
 list_1 <- gsub(".csv", "", gsub("./", "", data_MSC))
 
@@ -21,12 +21,12 @@ for (i in 1:length(data_MSC)) {
 }
 names(MSC_1) <- list_1
 MSC_2 <-rbindlist(MSC_1, use.names=TRUE, fill=TRUE, idcol="merged")
-write.csv(MSC_2, "~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE2/MSC_1stage.csv", row.names = F, quote = F)
+write.csv(MSC_2, "~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE2/MSC_1stage.csv", row.names = F, quote = F)
 
 #################
 # DM
 
-setwd("~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE1/2_DM/")
+setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE1/2_DM/")
 data_DM <- list.files(pattern = ".csv", full.names = T)
 list_2 <- gsub(".csv", "", gsub("./", "", data_DM))
 
@@ -38,12 +38,12 @@ for (i in 1:length(data_DM)) {
 }
 names(DM_1) <- list_2
 DM_2 <-rbindlist(DM_1, use.names=TRUE, fill=TRUE, idcol="merged")
-write.csv(DM_2, "~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE2/DM_1stage.csv", row.names = F, quote = F)
+write.csv(DM_2, "~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE2/DM_1stage.csv", row.names = F, quote = F)
 
 #################
 # Height
 
-setwd("~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE1/3_Height/")
+setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE1/3_Height/")
 data_Height <- list.files(pattern = ".csv", full.names = T)
 list_3 <- gsub(".csv", "", gsub("./", "", data_Height))
 
@@ -55,12 +55,12 @@ for (i in 1:length(data_Height)) {
 }
 names(Height_1) <- list_3
 Height_2 <-rbindlist(Height_1, use.names=TRUE, fill=TRUE, idcol="merged")
-write.csv(Height_1, "~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE2/Height_1stage.csv", row.names = F, quote = F)
+write.csv(Height_1, "~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE2/Height_1stage.csv", row.names = F, quote = F)
 
 #################
 # Yield
 
-setwd("~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE1/4_Yield/")
+setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE1/4_Yield/")
 data_Yield <- list.files(pattern = ".csv", full.names = T)
 list_4 <- gsub(".csv", "", gsub("./", "", data_Yield))
 
@@ -72,12 +72,12 @@ for (i in 1:length(data_Yield)) {
 }
 names(Yield_1) <- list_4
 Yield_2 <-rbindlist(Yield_1, use.names=TRUE, fill=TRUE, idcol="merged")
-write.csv(Yield_2, "~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE2/Yield_1stage.csv", row.names = F, quote = F)
+write.csv(Yield_2, "~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE2/Yield_1stage.csv", row.names = F, quote = F)
 
 #################
 # FD
 
-setwd("~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE1/5_FD/")
+setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE1/5_FD/")
 data_FD <- list.files(pattern = ".csv", full.names = T)
 list_2 <- gsub(".csv", "", gsub("./", "", data_FD))
 
@@ -89,7 +89,7 @@ for (i in 1:length(data_FD)) {
 }
 names(FD_1) <- list_2
 FD_2 <-rbindlist(FD_1, use.names=TRUE, fill=TRUE, idcol="merged")
-write.csv(FD_2, "~/Documents/Cesar/git/Norberg_2020/mr_bean_blue1/BLUE2/FD_1stage.csv", row.names = F, quote = F)
+write.csv(FD_2, "~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE2/FD_1stage.csv", row.names = F, quote = F)
 
 #################
 # end
