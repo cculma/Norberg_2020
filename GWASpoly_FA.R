8386542449d2441c43d43d747cb8cfd5cee2a03d
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-03-14 18:02:42 -0700

    add blue cov1

--- GWASpoly_FA.R
+++ GWASpoly_FA.R
@@ -21,6 +21,8 @@ pheno <- read.csv("~/Documents/Cesar/git/big_files/pheno_fa.csv", row.names = 1)
 head(pheno)
 trait1 <- colnames(pheno)[1:(length(colnames(pheno))-3)]
 trait1
+trait2 <- c("FA1_He_ID_2019_1","FA3_He_ID_2019_1","ST3_He_OR","ST3_He_OR1")
+trait2 <- c("FA1_He_ID_2019_1","FA3_He_ID_2019_1")
 params <- set.params(fixed=c("PC1","PC2","PC3"),
                      fixed.type=rep("numeric",3), n.PC = 3)
 models_1 <- c("general", "additive", "1-dom", "2-dom",  "diplo-additive", "diplo-general")
@@ -30,9 +32,17 @@ data_1 <- read.GWASpoly(ploidy=4,
                         geno.file="AllSamples_Ms_filter_q30_imputed_GWASPoly_contigRemoved.txt", 
                         format="ACGT", n.traits=length(trait1), delim=",")
 data_2 <- set.K(data = data_1, LOCO = F, n.core = 32)
-data_4.1 <- GWASpoly(data = data_2, models = models_1, traits = trait1, params = params, n.core = 32)
+# data_4.1 <- GWASpoly(data = data_2, models = models_1, traits = trait1, params = params, n.core = 32)
+data_4.1 <- GWASpoly(data = data_2, models = models_1, traits = "BLUE_He_ID_2019_1", params = params, n.core = 32)
 save(data_4.1, file = "~/Documents/Cesar/git/big_files/data_4.1.RData")
-data_5.3 <- set.threshold(data_4.1, method= "Bonferroni", level=0.05)
+data_5.3 <- set.threshold(data_4.1, method= "Bonferroni", level=0.2)
 data_6.4 <- get.QTL(data_5.3) 
 data_6.4 %>% distinct(Marker, .keep_all = T) 
 data_6.4$Marker
+hist(pheno$FA1_He_ID_2019_1)
+hist(pheno$FA3_He_ID_2019_1)
+cor(pheno$FA1_He_ID_2019_1, pheno$FA3_He_ID_2019_1)
+colnames(pheno)
+pheno[,c(1,11)]
+var(pheno$FA1_He_ID_2019_1)
+var(pheno$FA3_He_ID_2019_1)
--- tidy_stage.R
+++ tidy_stage.R
@@ -41,39 +41,34 @@ corrplot(f2, type="upper", method = 'number')
 ####################
 # 3 stage results (three locations)
 
-setwd("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/1_MSC/3_stage/")
 
-a_1_3 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/1_MSC/3_stage/3_Overall_predictions_2stage.csv")
-a_1_3 <- a_1_3 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_1_3)[2] <- "ST3_MS_WA"
+fa1 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE3_ar/ST1_He_FA1.csv")
+fa1 <- fa1 %>% dplyr::select(1:3) %>% spread(trial, predicted.value) %>% remove_rownames() %>% column_to_rownames(var = "gen")
+list_5 <- gsub("^", "FA1_He_", colnames(fa1))
+colnames(fa1) <- list_5
+fa1 <- fa1 %>% rownames_to_column(var = "gen")
 
-a_1_4 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/1_MSC/4_stage/Overall_predictions_2stage.csv")
+
+fa3 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE3_ar/ST1_He_FA3.csv")
+fa3 <- fa3 %>% dplyr::select(1:3) %>% spread(trial, predicted.value) %>% remove_rownames() %>% column_to_rownames(var = "gen")
+list_5 <- gsub("^", "FA3_He_", colnames(fa3))
+colnames(fa3) <- list_5
+fa3 <- fa3 %>% rownames_to_column(var = "gen")
+
+a_1_4 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE3_ar/ST3_OR_He.csv")
 a_1_4 <- a_1_4 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_1_4)[2] <- "ST4_MS"
+colnames(a_1_4)[2] <- "ST3_He_OR"
 
-a_2_4 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/2_DM/4_stage/Overall_predictions_2stage.csv")
-a_2_4 <- a_2_4 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_2_4)[2] <- "ST4_DM"
+a_1_5 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE3_ar/ST3_OR_He1.csv")
+a_1_5 <- a_1_5 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
+colnames(a_1_5)[2] <- "ST3_He_OR1"
 
-a_3_2 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/3_Height/3_stage/2_Overall_predictions_2stage.csv")
-a_3_2 <- a_3_2 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_3_2)[2] <- "ST3_He_OR"
-
-a_3_3 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/3_Height/3_stage/3_Overall_predictions_2stage.csv")
-a_3_3 <- a_3_3 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_3_3)[2] <- "ST3_He_WA"
-
-a_4_1 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/4_Yield/3_stage/1_Overall_predictions_2stage.csv")
-a_4_1 <- a_4_1 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_4_1)[2] <- "ST3_Yi_ID"
-
-a_4_2 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/4_Yield/3_stage/2_Overall_predictions_2stage.csv")
-a_4_2 <- a_4_2 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_4_2)[2] <- "ST3_Yi_OR"
-
-a_4_3 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/FA_1/4_Yield/3_stage/3_Overall_predictions_2stage.csv")
-a_4_3 <- a_4_3 %>% dplyr::select(1,2) %>% remove_rownames() %>% column_to_rownames(var = "gen") %>% rownames_to_column(var = "gen")
-colnames(a_4_3)[2] <- "ST3_Yi_WA"
+fa1 <- read.csv("~/Documents/Cesar/git/Norberg_2020/BLUE_values/BLUE3_ar/3_Height_1stage.csv")
+fa1 <- fa1 %>% dplyr::select(1:3) %>% spread(merged, predicted.value) %>% remove_rownames() %>% column_to_rownames(var = "gen")
+list_5 <- gsub("^", "BLUE_He_", colnames(fa1))
+colnames(fa1) <- list_5
+fa1 <- fa1 %>% rownames_to_column(var = "gen")
+f1 <- inner_join(fa1, PCA, by = "gen")
 
 ##~~~~~~~~~~~~~
 PCA <- read.csv("~/Documents/Cesar/git/big_files/pheno.csv")
@@ -82,7 +77,7 @@ PCA <- PCA %>% dplyr::select(c(1,109:111))
 str(PCA)
 PCA$gen <- as.character(PCA$gen)
 
-f1 <- inner_join(a_1_3, a_1_4, by = "gen") %>% inner_join(., a_2_4, by = "gen") %>% inner_join(., a_3_2, by = "gen") %>% inner_join(., a_3_3, by = "gen") %>% inner_join(., a_4_1, by = "gen") %>% inner_join(., a_4_2, by = "gen") %>% inner_join(., a_4_3, by = "gen") %>% inner_join(., PCA, by = "gen")
+f1 <- inner_join(fa1, fa3, by = "gen") %>% inner_join(., a_1_4, by = "gen") %>% inner_join(., a_1_5, by = "gen") %>% inner_join(., PCA, by = "gen")
 colnames(f1)
 write.csv(f1, "~/Documents/Cesar/git/big_files/pheno_fa.csv", quote = F, row.names = F)
 
